07d3b2aeefa84654566cc21d3524552cf658bd70
Jahnavi Patadia <jahnavipatadia112@gmail.com>
2024-05-30 11:53:36 +0530

    Cmpty Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import BaseButton from "./component/base/BaseButton";
+
 function App() {
   return (
     <div>
       <h1 className="h1">Hello world!</h1>
+      <BaseButton />
     </div>
   );
 }
