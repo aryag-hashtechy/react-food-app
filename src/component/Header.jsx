2a513c46870df29e48395a13fb12d129d0631e84
Jahnavi Patadia <jahnavipatadia112@gmail.com>
2024-06-04 11:00:37 +0530

    Fixed some CSS class name

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,13 @@
-import react from "react";
+import react, { useState } from "react";
 import logo from "../assets/images/Bella Olonje logo 111 1.svg";
+import { FiMenu } from "react-icons/fi";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
       <header className="header-main">
@@ -13,7 +19,19 @@ const Header = () => {
           <li>Faq</li>
           <li>Contact</li>
         </ul>
+
+        <FiMenu className="header-icon" onClick={toggleMenu} />
       </header>
+      {isOpen && (
+        <div>
+          <ul className="bg-white w-full pl-4 pr-2 md:hidden  pr-16 pb-4">
+            <li className="border-b-2 border-black">Home</li>
+            <li className="border-b-2 border-black ">Product</li>
+            <li className="border-b-2 border-black">Faq</li>
+            <li className="border-b-2 border-black">Contact</li>
+          </ul>
+        </div>
+      )}
     </>
   );
 };
