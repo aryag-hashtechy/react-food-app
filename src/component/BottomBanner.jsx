4102ed11ea3d5b0038e4241395114ecd578b9a57
Jahnavi Patadia <jahnavipatadia112@gmail.com>
2024-06-04 17:10:10 +0530

    Made BottomBanner and footer responsive

--- src/component/BottomBanner.jsx
+++ src/component/BottomBanner.jsx
@@ -6,19 +6,26 @@ const BottomBanner = () => {
   return (
     <>
       <div className="banner-main">
-        <img src={image} />
+        <img className="banner-image" src={image} />
         <div className="banner-text">
           <h1 className="h1">Download the app now.</h1>
           <h3 className="h3 font-mont tracking-[0.2px]">
             Available on your favourite store. Start your premium experience now
           </h3>
-          <div className="banner-btn">
+          <div className="banner-btn-window">
             <BaseButton
               buttonText={"Playstore"}
               variant={"btn-primary-square"}
             />
             <BaseButton
               buttonText={"App Store"}
+              variant={"btn-secondary-square"}
+            />
+          </div>
+          <div className="banner-btn-mobile">
+            <BaseButton buttonText={"Buy now"} variant={"btn-primary-square"} />
+            <BaseButton
+              buttonText={"Try for free"}
               variant={"btn-secondary-square"}
             />
           </div>
