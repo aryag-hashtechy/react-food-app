94af5eda0ea32fc3c36465269900b7e654487801
Jahnavi Patadia <jahnavipatadia112@gmail.com>
2024-06-05 15:10:46 +0530

    Completed bottom banner and footer responsive changes.

--- src/component/BottomBanner.jsx
+++ src/component/BottomBanner.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import image from "../assets/images/Rectangle 47.png";
+
 import BaseButton from "./base/BaseButton";
 
 const BottomBanner = () => {
   return (
     <>
       <div className="banner-main">
-        <img className="banner-image" src={image} />
         <div className="banner-text">
           <h1 className="h1">Download the app now.</h1>
           <h3 className="h3 font-mont tracking-[0.2px]">
             Available on your favourite store. Start your premium experience now
           </h3>
+
           <div className="banner-btn-window">
             <BaseButton
               buttonText={"Playstore"}
