1db58a2f0e6fc4c671362fc3cd7d6439cb8131ea
Dhruvish089 <dhruvishg.hashtechy@gmail.com>
2024-06-08 15:18:28 +0530

    implements splash screen

---
+++ src/component/Header.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import logo from "../assets/icons/logo.svg";
+import { FiMenu } from "react-icons/fi";
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <>
+      <header className="header-main">
+        <img src={logo} alt="logo" className="header-image" />
+
+        <ul className="header-list">
+          <li className="active">Home</li>
+          <li>Product</li>
+          <li>Faq</li>
+          <li>Contact</li>
+        </ul>
+
+        <FiMenu className="header-icon" onClick={toggleMenu} />
+      </header>
+      {isOpen && (
+        <div>
+          <ul className="hamburger-menu">
+            <li>Home</li>
+            <li>Product</li>
+            <li>Faq</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Header;
