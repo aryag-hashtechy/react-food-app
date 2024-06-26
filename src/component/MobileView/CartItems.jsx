dcfe8787fab23af790c2490d34bda3d2c9200f4d
Jahnavi Patadia <jahnavipatadia112@gmail.com>
2024-06-26 16:14:00 +0530

    Cart page design

--- src/component/MobileView/CartItems.jsx
+++ src/component/MobileView/CartItems.jsx
@@ -1,42 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import QuantityCounter from "./QuantityCounter";
 import image1 from "../../assets/images/image2.png";
 
-const cartItems = [
-  {
-    id: 1,
-    name: "Veggie tomato mix",
-    mrp: 29.99,
-    image: { image1 },
-  },
-  {
-    id: 2,
-    name: "Fish with mix orange",
-    mrp: 49.99,
-    image: { image1 },
-  },
-  {
-    id: 3,
-    name: "Veggie tomato mix",
-    mrp: 19.99,
-    image: { image1 },
-  },
-];
-
-const handleDelete = (id) => {
-  cartItems.map((item, index) => {
-    if (item.id === id) {
-      const deleteData = cartItems.splice(index, 1);
-      console.log(deleteData);
-    }
-  });
-};
+// const handleDelete = (id) => {
+//   cartItems.map((item, index) => {
+//     if (item.id === id) {
+//       const deleteData = cartItems.splice(index, 1);
+//       console.log(deleteData);
+//     }
+//   });
+// };
 
 const CartItems = () => {
+  const cartItemsV2 = [
+    {
+      id: 1,
+      name: "Veggie tomato mix",
+      mrp: 29.99,
+      image: { image1 },
+    },
+    {
+      id: 2,
+      name: "Fish with mix orange",
+      mrp: 49.99,
+      image: { image1 },
+    },
+    {
+      id: 3,
+      name: "Veggie tomato mix",
+      mrp: 19.99,
+      image: { image1 },
+    },
+  ];
+  const [cartItems, setCartItems] = useState(cartItemsV2);
+
+  const handleDelete = (id) => {
+    const updatedCartItems = cartItems.filter((item) => item.id !== id);
+    setCartItems(updatedCartItems);
+  };
+
   return (
     <section>
       <div className="cart__main">
-        {cartItems.map((item) => (
+        {cartItems?.map((item) => (
           <QuantityCounter
             key={item.id}
             id={item.id}
