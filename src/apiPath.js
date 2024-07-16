ec45a219569c56a6aac31038c3ed146e873b0522
Dhruvish089 <dhruvishg.hashtechy@gmail.com>
2024-07-16 11:06:32 +0530

    -made some changes

--- src/apiPath.js
+++ src/apiPath.js
@@ -3,20 +3,20 @@ const baseUrl = "http://localhost:5000";
 let apiPath = {
   signup: baseUrl + "/api/users/signup",
   signIn: baseUrl + "/api/users/signin",
-  getAllFood: baseUrl + "/product/getall-food",
+  getAllFood: baseUrl + "/api/product/getall-food",
   getUser: baseUrl + "/api/users/get-user",
   searchItems: baseUrl + "/api/users/search-items",
-  getSingleFood: baseUrl + "/product/getsingle-food",
+  getSingleFood: baseUrl + "/api/product/getsingle-food",
   updateAddress: baseUrl + "/api/address/update-address/",
   addAddress: baseUrl + "/api/address/add-address",
   deleteAddress: baseUrl + "/api/address/delete-address/",
   getSingleAddress: baseUrl + "/api/address/get-single-address",
   getAllAddress: baseUrl + "/api/address/getall-address",
-  createCart: baseUrl + '/cart/add-cart',
-  getCart: baseUrl + '/cart/get-cart',
+  createCart: baseUrl + '/api/cart/add-cart',
+  getCart: baseUrl + '/api/cart/get-cart',
   updateCart: baseUrl + '/cart/update-cart',
-  deleteCart: baseUrl + '/cart/delete-cart',
-  createOrder: baseUrl + '/order/create-order',
+  deleteCart: baseUrl + '/api/cart/delete-cart',
+  createOrder: baseUrl + '/api/order/create-order',
 };
 
 export default apiPath;
